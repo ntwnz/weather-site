0f4c303e11e976aad7db917c9d8ce1bbdc7cc018
ntwnz <isaacnotwen@protonmail.com>
2026-01-04 09:51:04 -0300

    push completo

---
+++ src/javascript/script.js
@@ -0,0 +1,65 @@
+document.querySelector('#search').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const cityName = document.querySelector('#city_name').value;
+
+    if(!cityName){
+        document.querySelector('#weather').classList.remove('show');
+        return showAlert(`Você deve digitar uma cidade`);
+    }
+    const apiKey = '82005d27a116c2880c8f0fcb866998a0';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&lang=pt_br&appid=${apiKey}&units=metric`;
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    console.log(json);
+    if(json.cod ===200){
+        showInfo({
+            city:json.name,
+            country:json.sys.country,
+            temp: json.main.temp,
+            tempMax: json.main.temp_max,
+            tempMin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempIcon: json.weather[0].icon,
+            windSpeed: json.wind.speed,
+            humidity: json.main.humidity,
+        })
+    } else{
+        document.querySelector('#weather').classList.remove('show');
+
+        showAlert(`
+            Cidade não localizada
+            
+            <img src="src/404.png"/>
+            
+            `);
+    }
+});
+
+function showInfo (json){
+    showAlert('')
+
+    document.querySelector('#weather').classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country} `
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace(".",",")} C° `
+
+    document.querySelector('#temp_max').innerHTML = `${json.tempMax.toFixed(1).toString().replace(".",",")} C°`
+
+    document.querySelector('#temp_min').innerHTML = `${json.tempMin.toFixed(1).toString().replace(".",",")} C°`
+
+    document.querySelector('#humidity').innerHTML = `${json.humidity} %`
+
+    document.querySelector('#wind').innerHTML = `${json.windSpeed.toFixed(1)} Km/h`
+
+    document.querySelector('#temp_description').innerHTML = `${json.description}`
+
+    document.querySelector('#temp_img').setAttribute("src", `https://openweathermap.org/img/wn/${json.tempIcon}@2x.png`)
+
+}
+
+function showAlert(msg) {
+    document.querySelector('#alert').innerHTML = msg;
+}
